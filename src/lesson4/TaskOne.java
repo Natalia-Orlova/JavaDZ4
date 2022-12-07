3903ab56a808931fd19eb412b97cf5097b7bb92e
NataliaOrlova <natakarelskaya@yandex.ru>
2022-12-07 21:32:42 +0400

    one more solution task1

--- src/lesson4/TaskOne.java
+++ src/lesson4/TaskOne.java
@@ -57,17 +57,14 @@ public class TaskOne {
 //    public int[][] updateMatrix(int[][] mat) {
 //
 //        Queue<int[]> q = new LinkedList<>();
-//        int[][] ans = new int[mat.length][mat[0].length];
-//        for (int[] num : ans) {
-//            Arrays.fill(num, -1);
-//        }
-//        System.out.println(ans);
 //
 //        for (int row = 0; row < mat.length; row++) {
 //            for (int col = 0; col < mat[0].length; col++) {
 //                if (mat[row][col] == 0) {
 //                    q.add(new int[] {row, col});
-//                    ans[row][col] = 0;
+//                }
+//                else {
+//                    mat[row][col] = Integer.MAX_VALUE;
 //                }
 //            }
 //        }
@@ -76,6 +73,7 @@ public class TaskOne {
 //            int[] curPoint = q.poll();
 //            int curRow = curPoint[0];
 //            int curCol = curPoint[1];
+//
 //            for (int[] direction : DIRECTIONS) {
 //                int newRow = curRow + direction[0];
 //                int newCol = curCol + direction[1];
